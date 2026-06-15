2eaf2515530c2d18f00c158262bdef1ec00895a6
jonas-abr <jonasdesenvolvedorfront@gmail.com>
2026-06-15 19:39:11 -0300

    first commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker básico para permitir a instalação do PWA offline
+const CACHE_NAME = 'bolos-amandy-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/logotipo.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
